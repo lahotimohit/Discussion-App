087133c93df7dcf7b9b6ff11468c420a95832d0e
Mohit Lahoti <engtechno25@gmail.com>
2024-10-27 00:26:16 +0530

    Initialisation of project

---
+++ lib/discussion_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule DiscussionWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use DiscussionWeb, :controller` and
+  `use DiscussionWeb, :live_view`.
+  """
+  use DiscussionWeb, :html
+
+  embed_templates "layouts/*"
+end
