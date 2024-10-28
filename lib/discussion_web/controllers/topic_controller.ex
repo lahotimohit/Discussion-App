d85e167e9a055df2da8767cb218ab1232bf1d501
Mohit Lahoti <engtechno25@gmail.com>
2024-10-28 23:27:31 +0530

    Creating form in phoenix live view with changeset rule

--- lib/discussion_web/controllers/topic_controller.ex
+++ lib/discussion_web/controllers/topic_controller.ex
@@ -2,7 +2,10 @@ defmodule DiscussionWeb.TopicController do
   use DiscussionWeb, :controller
 
   def new(conn, _params) do
-    render(conn, :new)
+    struct = %Discussion.Topics.Topic{}
+    params = %{}
+    changeset = Discussion.Topics.Topic.changeset(struct, params)
+    render(conn, :new, changeset: changeset)
   end
 
 end
